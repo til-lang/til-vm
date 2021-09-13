98486a4e0d6a183094e384cc05d56b12358df21b
Cléber Zavadniak <contato@cleber.solutions>
2021-09-12 23:02:25 -0300

    Test script working properly

--- source/app.d
+++ source/app.d
@@ -12,6 +12,8 @@ enum Opcode
 {
     push,
     push_from_bp,
+    add_to_bp,
+    ret,
 
     call_proc,
     call_primitive,
@@ -22,16 +24,35 @@ struct Instruction
 {
     Opcode opcode;
     size_t arg1;
+    string comment;
 
+    this(Opcode opcode)
+    {
+        this(opcode, 0);
+    }
+    this(Opcode opcode, string comment)
+    {
+        this(opcode, 0, comment);
+    }
     this(Opcode opcode, size_t arg1)
+    {
+        this(opcode, arg1, "");
+    }
+    this(Opcode opcode, size_t arg1, string comment)
     {
         this.opcode = opcode;
         this.arg1 = arg1;
+        this.comment = comment;
     }
 
     string toString()
     {
-        return to!string(this.opcode) ~ " " ~ to!string(this.arg1);
+        auto s = to!string(this.opcode) ~ " " ~ to!string(this.arg1);
+        if (this.comment.length > 0)
+        {
+            s ~= "  # " ~ this.comment;
+        }
+        return s;
     }
 }
 
@@ -46,6 +67,10 @@ struct Proc
     {
         this.name = name;
         this.routine = routine;
+    }
+    string toString()
+    {
+        return this.name;
     }
 }
 
@@ -65,6 +90,7 @@ class VM : ListItem
     SubList body;
     ProcsDict procs;
     int procsCount = 0;
+    ulong code_pointer = 0;
 
     this(string name, SimpleList parameters, SubList body)
     {
@@ -79,16 +105,26 @@ class VM : ListItem
     }
     Routine compile(SubProgram subprogram, Items parameters, int level)
     {
+        return compile(subprogram, parameters, 0, level);
+    }
+    Routine compile(
+        SubProgram subprogram,
+        Items parameters,
+        size_t nextBPOffset,
+        int level
+    )
+    {
         Routine routine;
 
         size_t[string] parameterNames;
-        foreach(index, parameter; parameters)
+        foreach (index, parameter; parameters)
         {
             // a b c   .length = 3
             // 0 1 2   index
             // 2 1 0
             auto name = parameter.toString();
-            parameterNames[name] = parameters.length - index - 1;
+            //parameterNames[name] = parameters.length - index - 1;
+            parameterNames[name] = index;
         }
 
         string spacer = "";
@@ -97,10 +133,9 @@ class VM : ListItem
             spacer ~= " ";
         }
 
-        foreach(pipeline; subprogram.pipelines)
+        foreach (pipeline; subprogram.pipelines)
         {
-command:
-            foreach(command; pipeline.commands)
+            foreach (command; pipeline.commands)
             {
                 if (command.name == "proc")
                 {
@@ -109,7 +144,7 @@ command:
                 }
 
                 // Evaluate all arguments:
-                foreach(argument; command.arguments.retro)
+                foreach (index, argument; command.arguments)
                 {
                     switch(argument.type)
                     {
@@ -118,9 +153,12 @@ command:
                                 "VM should not compile a SubProgram"
                             );
                         case ObjectType.ExecList:
+                            // The SubProgram itself is going
+                            // to save and restore its own BP:
                             routine ~= compile(
                                 (cast(ExecList)argument).subprogram,
                                 parameters,
+                                nextBPOffset + 1,
                                 level + 1
                             );
                             break;
@@ -128,10 +166,12 @@ command:
                             auto n = argument.toString();
                             if (auto countPointer = n in parameterNames)
                             {
-                                // TODO: define the call stack properly!
                                 routine ~= Instruction(
-                                    Opcode.push_from_bp, *countPointer
+                                    Opcode.push_from_bp,
+                                    *countPointer,
+                                    "$" ~ n
                                 );
+                                nextBPOffset += 1;
                             }
                             else
                             {
@@ -146,29 +186,46 @@ command:
                             );
                     }
                 }
+
                 // Check if it's a call to a user-defined procedure:
-                foreach(entry; this.procs.byPair)
+                bool isProc = false;
+                foreach (entry; this.procs.byPair)
                 {
                     if (command.name == entry.value.name)
                     {
-                        routine ~= compileProcCall(entry.key, level);
-                        break command;
-                    }
-                }
+                        isProc = true;
 
-                // Check if it's a call to a primitive word:
-                for (int i = 0; i <= Dictionary.max; i++)
-                {
-                    if (to!Dictionary(command.name) == i)
-                    {
-                        routine ~= compilePrimitiveWordCall(
-                            i, level
+                        routine ~= Instruction(Opcode.add_to_bp, nextBPOffset);
+                        routine ~= Instruction(
+                            Opcode.call_proc, entry.key, command.name
                         );
-                        break command;
+                        routine ~= Instruction(
+                            Opcode.ret,
+                            nextBPOffset,
+                            "return from " ~ command.name
+                            ~ " with offset " ~ to!string(nextBPOffset)
+                        );
+
+                        break;
                     }
                 }
-            }
+                if (!isProc)
+                {
+                    // Check if it's a call to a primitive word:
+                    for (int i = 0; i <= Dictionary.max; i++)
+                    {
+                        if (to!Dictionary(command.name) == i)
+                        {
+                            routine ~= Instruction(
+                                Opcode.call_primitive, i, command.name
+                            );
+                            break;
+                        }
+                    }
+                }
+            } // end foreach command
         }
+
         return routine;
     }
     void compileProc(Command command, int level)
@@ -187,14 +244,6 @@ command:
         Routine routine = compile(body.subprogram, parameters.items);
         this.addProc(name, routine);
     }
-    Routine compileProcCall(size_t index, int level)
-    {
-        return [Instruction(Opcode.call_proc, index)];
-    }
-    Routine compilePrimitiveWordCall(int index, int level)
-    {
-        return [Instruction(Opcode.call_primitive, index)];
-    }
 
     void addProc(string name, Routine routine)
     {
@@ -205,97 +254,113 @@ command:
             writeln(" ", instruction);
         }
     }
-    void execute(Routine routine, Items arguments)
+    size_t execute(Routine routine, Items arguments)
     {
         size_t[64] stack;
         size_t BP, SP = 0;
 
-        void printStack()
+        void printStack(string prefix)
         {
-            string s = "[ ";
-            for (auto i = 0; i < SP; i++)
+            string s = prefix ~ "[ ";
+            for (auto i = 0; i <= SP; i++)
             {
-                s ~= to!string(stack[i]);
                 if (i == BP)
                 {
-                    s ~= ":BP ";
+                    s ~= "(" ~ to!string(stack[i]) ~ ") ";
                 }
                 else
                 {
-                    s ~= " ";
+                    s ~= to!string(stack[i]) ~ " ";
                 }
             }
-            s ~= "]";
+            s ~= "] " ~ to!string(SP);
             writeln(s);
         }
 
         // Fill the stack with the execution arguments:
+        SP -= 1;
         foreach (argument; arguments)
         {
-            stack[SP++] = argument.toInt();
+            stack[++SP] = argument.toInt();
         }
-        printStack();
+        printStack("");
         writeln("= go! =");
 
         size_t pop()
         {
-            return stack[--SP];
+            return stack[SP--];
         }
         void push(size_t value)
         {
-            stack[SP++] = value;
+            stack[++SP] = value;
         }
 
-        // TODO: load user-defined routines (procs)
-        // into memory before starting the program execution.
-        // We DON'T wanna call `execute` recursively,
-        // because we really want the "registers" to be
-        // local variables, so that they are mapped
-        // directly by the D compiler into... actual registers.
-
-        // TODO: this is going to become a while (true)
-        // with increment of IP.
-        foreach (instruction; routine)
+        void executeRoutine(Routine routine)
         {
-            final switch (instruction.opcode)
+            writeln("= executeRoutine =");
+            foreach (instruction; routine)
             {
-                case Opcode.push:
-                    push(instruction.arg1);
-                break;
-                case Opcode.push_from_bp:
-                    push(stack[BP + instruction.arg1]);
-                break;
-                case Opcode.call_primitive:
-                    switch (instruction.arg1)
-                    {
-                        case Dictionary.sum:
-                            auto a = stack[SP];
-                            auto b = stack[SP - 1];
-                            SP -= 1;
-                            stack[SP] = a + b;
-                            break;
-                        case Dictionary.mul:
-                            auto a = stack[SP];
-                            auto b = stack[SP - 1];
-                            SP -= 1;
-                            stack[SP] = a * b;
-                            break;
-                        default:
-                            throw new Exception(
-                                "Unknown opcode:" ~ to!string(instruction.arg1)
-                            );
-                    }
-                break;
-                case Opcode.call_proc:
-                    // auto routine = this.procs[instruction.arg1];
-                    // TODO: here is where the fun begins...
-                    writeln("(Not implemented yet)");
-                break;
-            }
-            writeln(instruction);
-            printStack();
+                writeln(instruction);
+                printStack("    ");
 
-        }
+                final switch (instruction.opcode)
+                {
+                    case Opcode.push:
+                        push(instruction.arg1);
+                    break;
+                    case Opcode.push_from_bp:
+                        push(stack[BP + instruction.arg1]);
+                    break;
+                    case Opcode.add_to_bp:
+                        BP += instruction.arg1;
+                    break;
+                    case Opcode.ret:
+                        // Put the last pushed value
+                        // into BP (return value address):
+                        stack[BP] = stack[SP];
+                        // Ignore everything else:
+                        SP = BP;
+                        // Move BP to the old position:
+                        BP -= instruction.arg1;
+                    break;
+                    case Opcode.call_primitive:
+                        switch (instruction.arg1)
+                        {
+                            case Dictionary.sum:
+                                auto a = stack[SP];
+                                auto b = stack[SP - 1];
+                                SP -= 1;
+                                stack[SP] = a + b;
+                                break;
+                            case Dictionary.mul:
+                                auto a = stack[SP];
+                                auto b = stack[SP - 1];
+                                SP -= 1;
+                                stack[SP] = a * b;
+                                break;
+                            default:
+                                throw new Exception(
+                                    "Unknown opcode:"
+                                    ~ to!string(instruction.arg1)
+                                );
+                        }
+                    break;
+                    case Opcode.call_proc:
+                        auto proc = this.procs[instruction.arg1];
+                        writeln("Executing procedure ", proc);
+                        executeRoutine(proc.routine);
+                    break;
+                }
+                printStack(" -> ");
+            } // end for instruction in routine
+            writeln("==================");
+        } // end executeRoutine
+
+        // "main" code of this function:
+        executeRoutine(routine);
+
+        printStack("STACK: ");
+        return stack[0];
     }
 
     CommandContext run(string path, CommandContext context)
@@ -304,10 +369,15 @@ command:
         auto routine = this.compile(
             this.body.subprogram, this.parameters.items
         );
+        // Force the main program to return properly:
+        routine ~= Instruction(
+            Opcode.ret,
+            0,
+            "return from main routine"
+        );
         writeln("Main program:");
-        this.execute(routine, arguments);
-        // TESTE:
-        return context.ret(new IntegerAtom(7));
+        auto value = this.execute(routine, arguments);
+        return context.ret(new IntegerAtom(value));
     }
 }
 
